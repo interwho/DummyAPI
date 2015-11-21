106e9f64aba1a93e4b020d22a886a5fdac3010cb
Justin Paulin <hello@justinpaulin.com>
2015-11-21 17:26:51 -0600

    Added extra routes

--- config/Routes.php
+++ config/Routes.php
@@ -17,4 +17,57 @@ $collection->add(
     )
 );
 
+// API
+$collection->add(
+    'PageController_usersGet',
+    new Route(
+        '/api/users',
+        array(
+            '_controller' => 'RequestCity\DummyAPI\Controllers\PageController::usersGet'
+        ),
+        array(
+            '_method' => 'GET'
+        )
+    )
+);
+
+$collection->add(
+    'PageController_authenticationGet',
+    new Route(
+        '/api/authentication',
+        array(
+            '_controller' => 'RequestCity\DummyAPI\Controllers\PageController::authenticationGet'
+        ),
+        array(
+            '_method' => 'GET'
+        )
+    )
+);
+
+$collection->add(
+    'PageController_postsGet',
+    new Route(
+        '/api/posts',
+        array(
+            '_controller' => 'RequestCity\DummyAPI\Controllers\PageController::postsGet'
+        ),
+        array(
+            '_method' => 'GET'
+        )
+    )
+);
+
+$collection->add(
+    'PageController_loginGet',
+    new Route(
+        '/api/login',
+        array(
+            '_controller' => 'RequestCity\DummyAPI\Controllers\PageController::loginGet'
+        ),
+        array(
+            '_method' => 'GET'
+        )
+    )
+);
+
 return $collection;
--- src/RequestCity/DummyAPI/Controllers/PageController.php
+++ src/RequestCity/DummyAPI/Controllers/PageController.php
@@ -11,6 +11,26 @@ class PageController extends Controller
 {
     public function root()
     {
-        return $this->createResponse('pong', Response::HTTP_CONTINUE);
+        return $this->createResponse('pong', Response::HTTP_OK);
+    }
+
+    public function usersGet()
+    {
+        return $this->createResponse('pong', Response::HTTP_OK);
+    }
+
+    public function authenticationGet()
+    {
+        return $this->createResponse('pong', Response::HTTP_OK);
+    }
+
+    public function postsGet()
+    {
+        return $this->createResponse('pong', Response::HTTP_OK);
+    }
+
+    public function loginGet()
+    {
+        return $this->createResponse('pong', Response::HTTP_OK);
     }
 }
